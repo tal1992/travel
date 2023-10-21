2316a55c7851e561f87afe813cf6f498da3a7bec
Talvinder Singh <talvindersingh1992@gmail.com>
2023-10-21 12:18:11 +0530

    fix: Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,23 @@ import "../../styles/main.css";
 import "../../styles/custom.css";
 import React from "react";
 import Script from "next/script";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link
-          href="https://www.googletagmanager.com/gtag/js?id=G-VP8T876JDG"
-          rel="preload"
-          as="script"
-        />
-      </Head>
       <Script
-        id="google-analytics"
+        src="https://www.googletagmanager.com/gtag/js?id=G-VP8T876JDG"
         strategy="afterInteractive"
-        src="/google-analytics.js"
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VP8T876JDG');
+        `}
+      </Script>
 
       <Component {...pageProps} />
     </>
